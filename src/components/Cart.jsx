5350565b7236f66cdb6f8df677c3c8aeeed4f5c0
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-10-16 22:26:19 -0600

    Add Cart Component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,6 +2,29 @@ import "./Cart.css";
 import { useState } from "react";
 import PropTypes from "prop-types";
 import Header from "./Header";
+import CartItem from "./CartItem";
+
+const sampleData = {
+  itemName: "Victoria",
+  price: 50,
+  amount: 1,
+  imageUrl: "/img/victoria-240x240.webp",
+  onAddToCart: () => console.log("Added to cart"),
+};
+const sampleData2 = {
+  itemName: "Azulito",
+  price: 100,
+  amount: 1,
+  imageUrl: "/img/azulito-240x240.webp",
+  onAddToCart: () => console.log("Added to cart"),
+};
+const sampleData3 = {
+  itemName: "Mojito",
+  price: 100,
+  amount: 1,
+  imageUrl: "/img/mojito-240x240.webp",
+  onAddToCart: () => console.log("Added to cart"),
+};
 
 function Cart({ onBack }) {
   const [cartItems, setCartItems] = useState(0);
@@ -22,6 +45,30 @@ function Cart({ onBack }) {
           cartItems={cartItems}
           title={"Carrito"}
         />
+        <CartItem
+          itemName={sampleData.itemName}
+          price={sampleData.price}
+          amount={sampleData.amount}
+          imageUrl={sampleData.imageUrl}
+          onAddToCart={sampleData.onAddToCart}
+        />
+        <CartItem
+          itemName={sampleData2.itemName}
+          price={sampleData2.price}
+          amount={sampleData2.amount}
+          imageUrl={sampleData2.imageUrl}
+          onAddToCart={sampleData2.onAddToCart}
+        />
+        <CartItem
+          itemName={sampleData3.itemName}
+          price={sampleData3.price}
+          amount={sampleData3.amount}
+          imageUrl={sampleData3.imageUrl}
+          onAddToCart={sampleData3.onAddToCart}
+        />
+
+        <p className="cart-subtotal">Subtotal: $250</p>
+
         <div className="go-to-cart">
           <button className="go-to-cart-button back-button" onClick={onBack}>
             Atrás
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+import "./CartItem.css";
+
+const MenuItem = ({ itemName, price, amount, imageUrl, onAddToCart }) => {
+  return (
+    <div className="cart-menu-item">
+      <div className="cart-menu-image">
+        <img src={imageUrl} alt={itemName} />
+      </div>
+      <div className="cart-menu-details">
+        <p>{itemName}</p>
+        <p>${price}</p>
+
+        <div className="cart-menu-controls">
+          <button onClick={onAddToCart}>-</button>
+          <p>{amount}</p>
+          <button onClick={onAddToCart}>+</button>
+        </div>
+
+        <button className="cart-menu-remove">Eliminar</button>
+      </div>
+    </div>
+  );
+};
+
+MenuItem.propTypes = {
+  itemName: PropTypes.func.isRequired,
+  price: PropTypes.func.isRequired,
+  amount: PropTypes.func.isRequired,
+  imageUrl: PropTypes.func.isRequired,
+  onAddToCart: PropTypes.func.isRequired,
+};
+
+export default MenuItem;
