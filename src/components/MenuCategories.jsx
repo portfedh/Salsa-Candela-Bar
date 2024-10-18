6eda36f5273657fd35d881039169073bf563f2b1
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-10-18 13:28:02 -0600

    Refactor MenuCategories component to use external categories data

--- src/components/MenuCategories.jsx
+++ src/components/MenuCategories.jsx
@@ -1,17 +1,5 @@
 import PropTypes from "prop-types";
-
-const categories = [
-  { id: "vodka-section", label: "Vodka $100" },
-  { id: "ron-section", label: "Ron $100" },
-  { id: "tequila-section", label: "Tequila $100" },
-  { id: "whiskey-section", label: "Whiskey $100" },
-  { id: "cervezas-section", label: "Cervezas $50" },
-  { id: "refrescos-section", label: "Refrescos $30" },
-  { id: "mocteles-section", label: "Mocteles $50" },
-  { id: "jugos-section", label: "Jugos $30" },
-  { id: "aguas-section", label: "Aguas $20" },
-  { id: "botanas-section", label: "Botanas $20" },
-];
+import categories from "../utils/categories";
 
 const MenuCategories = ({ scrollToSection }) => (
   <div className="menu-categories">
