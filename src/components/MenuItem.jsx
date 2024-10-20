059cf9a99bc32495bfc1eb42ac57895831cb3ada
Portfedh <59422723+portfedh@users.noreply.github.com>
2024-10-20 11:58:24 -0600

    Add comments to cart logic

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -7,15 +7,23 @@ const MenuItem = ({ id, itemName, price, amount, imageUrl, onAddToCart }) => {
   const [, , cartItems, setCartItems] = useContext(Context);
 
   const handleAddToCart = () => {
+    // Find the item in the cart that matches the current item's id
     const existingItem = cartItems.find((item) => item.id === id);
 
     if (existingItem) {
+      // If the item exists in the cart, update its quantity
       setCartItems(
         cartItems.map((item) =>
-          item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+          // Check if the current item in the map has the same id as the item being added
+          item.id === id
+            ? // If it does, return a new object with the same properties but increment the quantity by 1
+              { ...item, quantity: item.quantity + 1 }
+            : // If it doesn't, return the item as is
+              item
         )
       );
     } else {
+      // If the item does not exist in the cart, add it with a quantity of 1
       setCartItems([
         ...cartItems,
         { id, itemName, price, amount, imageUrl, quantity: 1 },
